f15313fd2a392fae794a6dab33952d4f96139eae
cpasjuste <cpasjuste@gmail.com>
2025-05-14 11:16:10 +0200

    add chained example

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 //
-// Created by cpasjuste on 31/03/25.
+// 74HC165 - cpasjuste @ 2025
 //
 
 #include <stdio.h>
@@ -7,6 +7,7 @@
 //#include <pico/stdio_usb.h>
 #include <pico/time.h>
 #include "74hc165.pio.h"
+#include "74hc165-x2.pio.h"
 
 #define DATA_PIN    2 // QH
 #define CLK_PIN     3 // CLK
@@ -15,7 +16,15 @@
 void print_bits(const uint8_t value) {
     printf("0x%02X: ", value);
     for (int i = 0; i < 8; i++) {
-        printf("%d", (value >> i) & 1);
+        printf("%d", value >> i & 1);
+    }
+    printf("\r\n");
+}
+
+void print_bits_16(const uint16_t value) {
+    printf("0x%04X: ", value);
+    for (int i = 0; i < 16; i++) {
+        printf("%d", value >> i & 1);
     }
     printf("\r\n");
 }
@@ -28,12 +37,17 @@ int main() {
 
     printf("Hello 74hc165\r\n");
 
-    hc165_t hc;
-    hc165_program_init(&hc, DATA_PIN, CLK_PIN, LATCH_PIN, 10);
+    // 1 chip
+    //hc165_t hc;
+    //hc165_program_init(&hc, DATA_PIN, CLK_PIN, LATCH_PIN, 10);
+
+    // 2 chips
+    hc165x2_t hc;
+    hc165x2_program_init(&hc, DATA_PIN, CLK_PIN, LATCH_PIN, 10);
 
     while (1) {
-        const uint8_t value = hc165_get(&hc);
-        print_bits(value);
+        const uint16_t value = hc165x2_get(&hc);
+        print_bits_16(value);
     }
 
     return 0;
